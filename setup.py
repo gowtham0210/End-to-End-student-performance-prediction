27bf56b79280375be139a5f62893a3e4fc12f7ac
Gowtham <49744195+gowtham0210@users.noreply.github.com>
2024-03-01 17:30:56 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+
+def get_requirments(file_path:str)->List[str]:
+    '''This function will return a list of requirements'''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n"," ") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+
+
+setup(
+    name='mlproject-1',
+    version='0.0.1',
+    author='Gowtham',
+    author_email='gowthambujju02@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirments('requirements.txt'),
+
+
+)
